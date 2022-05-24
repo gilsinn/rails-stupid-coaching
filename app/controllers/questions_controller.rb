249f306c861f0fc2c537484f55471ae5d37bc94f
Gil Singh <gilsinghsg@gmail.com>
2022-05-24 13:13:55 +0800

    Done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    answer_array = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+
+    @question =  params[:question]
+    #   @members = @members. { |member| member.start_with?(params[:question]) }
+    # end
+    # if input is "I am going to work" - answer is "Great!"
+    if @question == "I am going to work"
+      @answer = answer_array[0]
+    elsif @question.end_with?("?")
+      @answer = answer_array[1]
+    else
+      @answer = answer_array[2]
+    end
+    # if input ends with "?" - answer is "Silly question, get dressed and go to work!"
+
+    # else answer will be "I don't care, get dressed and go to work!"
+  end
+end
